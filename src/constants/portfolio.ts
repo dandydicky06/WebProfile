cbf268d0036627ecd97f083c37308a62fe1440be
dandydicky06 <154875940+dandydicky06@users.noreply.github.com>
2023-12-28 00:35:52 +0700

    update portofolio

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -6,7 +6,7 @@ export const portfolioList: Portfolio[] = [
     imageThumbnail: "/assets/images/webpribadi.webp",
     description:
       "This is a personal branding project that I created using Next JS, Tailwind CSS, and Framer Motion. This project consists of a website that showcases my skills, achievements, and personality. The website has seven sections: Home, About, Experiences, Services, Portfolio, Articles, and Contact. The website is designed to be responsive, interactive, and engaging. The website aims to impress and connect with potential clients, employers, and collaborators.",
-    livePreviewURL: "",
+    livePreviewURL: "https://dandydicky.vercel.app/",
     repositoryURL: "https://github.com/dandydicky06/WebProfile",
     techStack: [
       {
@@ -25,11 +25,11 @@ export const portfolioList: Portfolio[] = [
   },
   {
     title: "CoomingSoon!!!",
-    imageThumbnail: "/assets/images/Coomingsoon.webp",
+    imageThumbnail: "/assets/images/project1.webp",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo excepturi sint, fuga molestiae omnis at sunt tenetur distinctio odit adipisci ratione architecto atque vel fugiat eaque vitae qui officia error?Recusandae ipsa qui vero magni pariatur laboriosam sit similique animi expedita minima consequatur obcaecati, dolore possimus, consectetur, cupiditate sint! Similique sint in laborum magni corrupti aut consectetur cumque dolor earum!",
-    livePreviewURL: "",
-    repositoryURL: "https://github.com/dandydicky06",
+      "Zyra is a website that I created using HTML, CSS, and JavaScript. This website displays information about a company that sells phones with the Zyra brand. This website also showcases the features, specifications, and prices of the Zyra phones, as well as the customer reviews and feedback. This website aims to attract and inform potential buyers about the quality and innovation of the Zyra phones.",
+    livePreviewURL: "https://zyra-smartphone.vercel.app/",
+    repositoryURL: "https://github.com/dandydicky06/zyra_smartphone",
     techStack: [
     ],
   },
